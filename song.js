1617f6aa787d36d13844f7d84750457362181652
HeleneGreat <helene.dev@yahoo.com>
2022-07-03 21:03:56 +0200

    un peu de style

--- song.js
+++ song.js
@@ -5,23 +5,28 @@ btn.addEventListener('click', function(){
     let partOne = document.getElementById('part-one');
     let partTwo = document.getElementById('part-two');
     let nb = document.getElementById("quantity").value;
+    let quotes = document.querySelectorAll('.quote');
 
     // In case no number is entered
     if(nb === ""){
         partOne.innerText = "Si tu ne renseigne pas de quantité, je ne peux pas chanter !";
         partTwo.innerText = "";
-    // If the number entered is between 0 and 99 included
+        quotes.forEach(quote => {quote.classList.add('display-none')});
+    // If the number entered is between 1 and 99 included
     }else if(nb > 0 && nb <= 99){
         partOne.innerText = songPartOne(nb);
         partTwo.innerText = songPartTwo(nb - 1);
-    // If the number if zero, the song is different
+        quotes.forEach(quote => {quote.classList.remove('display-none')});
+        // If the number if zero, the song is different
     }else if(nb == 0){
         partOne.innerText = "Plus de bolées de cidre sur le mur, plus de bolées sans alcool.";
         partTwo.innerText = "Vas au supermarché pour en acheter, 99 bolées de cidre sur le mur.";
-    // If the number is not within 0 and 99
+        quotes.forEach(quote => {quote.classList.remove('display-none')});
+        // If the number is not within 0 and 99
     }else if(nb < 0 || nb > 99){
         partOne.innerText = "La quantité doit être comprise entre 0 et 99.";
         partTwo.innerText = "";
+        quotes.forEach(quote => {quote.classList.add('display-none')});
     }
   
 });
